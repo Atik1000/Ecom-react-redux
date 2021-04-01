16ffd0180c17f41eddb6b894df66e599df5f8778
Atik1000 <foysalkazi45@gmail.com>
2021-04-02 01:29:03 +0600

    added

---
+++ src/components/navigation/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Toolbar, Container, Grid, MenuItem } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const Navigation = () => {
+  const { count } = useSelector((state) => state.cartStore);
+  const history = useHistory();
+  const routePage = (url) => {
+    history.push(url);
+  };
+  return (
+    <>
+      <Container>
+        <Toolbar>
+          <Grid container justify="flex-start">
+            <Grid item>
+              <MenuItem onClick={() => routePage("/")}> Home</MenuItem>
+            </Grid>
+
+            <Grid item>
+              <MenuItem onClick={() => routePage("/cart")}>Cart</MenuItem>
+            </Grid>
+        
+          </Grid>
+
+          <Grid container justify="flex-end">
+            <Grid item>
+              <MenuItem onClick={() => routePage("/")}>
+                Total Item:{count}
+              </MenuItem>
+              
+            </Grid>
+            <Grid item>
+              <MenuItem onClick={() => routePage("/Sign in")}>Sign in</MenuItem>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </Container>
+    </>
+  );
+};
+
+export default Navigation;
---
+++ src/containers/product_details/index.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  CircularProgress,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+
+import { storeSingleProduct } from "../../store/action/productAction";
+const useStyles = makeStyles({
+  root: {
+    marginTop: 20,
+  },
+});
+
+const ProductDetail = () => {
+  const { count, productList } = useSelector((state) => state.cartStore);
+  const dispatch = useDispatch();
+  const { selectedProduct } = useSelector((state) => state.productStore);
+  const classes = useStyles();
+  const params = useParams();
+  const [loading, setLoading] = useState(true);
+  let { id } = params;
+  useEffect(() => {
+    async function getData() {
+      try {
+        const { data } = await axios.get(`http://54.162.199.74/products/${id}`);
+
+        dispatch(storeSingleProduct(data));
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getData();
+  }, []);
+  const addToCart = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        count: count ? count + 1 : 1,
+        productList: productList
+          ? productList.concat(selectedProduct)
+          : [...selectedProduct],
+      },
+    });
+  };
+  return (
+    <Container>
+      {loading && (
+        <CircularProgress style={{ marginLeft: "50%", marginTop: "30%" }} />
+      )}
+      {selectedProduct.hasOwnProperty("title") && (
+        <Card className={classes.root}>
+          <CardMedia
+            component="img"
+            alt={selectedProduct.title}
+            height="400"
+            image={selectedProduct.image}
+            title={selectedProduct.title}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {selectedProduct.title}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              {selectedProduct.description}
+              {selectedProduct.category}
+              <br />
+            </Typography>
+            <Typography variant="h6" color="primary" component="p">
+              price: ${selectedProduct.price}
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              variant="contained"
+              onClick={addToCart}
+            >
+              Add To Cart
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+    </Container>
+  );
+};
+
+export default ProductDetail;
