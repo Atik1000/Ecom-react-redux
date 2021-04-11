77513a2f41d25e12cc0d0bbfb54e4f9f90fc8232
Atik1000 <foysalkazi45@gmail.com>
2021-04-12 01:40:13 +0600

    fixed admin panel problem

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -15,25 +15,44 @@ const Navigation = () => {
         <Toolbar>
           <Grid container justify="flex-start">
             <Grid item>
-              <MenuItem onClick={() => routePage("/")}> FakeEcom</MenuItem>
+              <MenuItem onClick={() => routePage("/")}>
+                {" "}
+                <svg
+                  class="fill-current text-gray-800 mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M5,22h14c1.103,0,2-0.897,2-2V9c0-0.553-0.447-1-1-1h-3V7c0-2.757-2.243-5-5-5S7,4.243,7,7v1H4C3.447,8,3,8.447,3,9v11 C3,21.103,3.897,22,5,22z M9,7c0-1.654,1.346-3,3-3s3,1.346,3,3v1H9V7z M5,10h2v2h2v-2h6v2h2v-2h2l0.002,10H5V10z"></path>
+                </svg>{" "}
+                Bunon-world
+              </MenuItem>
             </Grid>
 
-            <Grid item></Grid>
+            
             <input
               type="text"
               placeholder="serch product"
-              style={{ width: "300px",borderRadius:'10px' }}
+              style={{ width: "300px", borderRadius: "10px" }}
             />
           </Grid>
 
           <Grid container justify="flex-end">
             <Grid item>
               <MenuItem onClick={() => routePage("/cart")}>
-                <img
-                  style={{ height: "30px", width: "30px" }}
-                  src="https://image.flaticon.com/icons/png/512/126/126083.png"
-                  alt=""
-                />
+                <svg
+                  class="fill-current hover:text-black"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21,7H7.462L5.91,3.586C5.748,3.229,5.392,3,5,3H2v2h2.356L9.09,15.414C9.252,15.771,9.608,16,10,16h8 c0.4,0,0.762-0.238,0.919-0.606l3-7c0.133-0.309,0.101-0.663-0.084-0.944C21.649,7.169,21.336,7,21,7z M17.341,14h-6.697L8.371,9 h11.112L17.341,14z"></path>
+                  <circle cx="10.5" cy="18.5" r="1.5"></circle>
+                  <circle cx="17.5" cy="18.5" r="1.5"></circle>
+                </svg>
+
                 <span style={{ color: "black" }}>{count}</span>
               </MenuItem>
             </Grid>
